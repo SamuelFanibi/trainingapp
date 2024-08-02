88a6a5fc282999b1be02146ce673a8eb9147530b
SamuelFanibi <s.fanibi@samfancy.com>
2024-08-02 19:52:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Samfancy <code>src/App.js</code> training class for August
+          Samfancy <code>src/App.js</code> training class for August Created by Samuel Fanibi
         </p>
         <a
           className="App-link"
